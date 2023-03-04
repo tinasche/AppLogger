02207218531bf916d5a95f29b2950440bb3e3c47
Tinashe Chitakunye <tinashec93@live.com>
2023-03-04 07:24:37 +0200

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogger
+{
+    internal class Logger
+    {
+        public void Log(string text)
+        {
+            Console.BackgroundColor= ConsoleColor.DarkGreen;
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.WriteLine(text);
+            Console.BackgroundColor = default;
+            Console.ForegroundColor = default;
+        }
+    }
+}
